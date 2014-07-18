30f66c2d83b7a287b1f0af481eebe971d46ab0c0
mattsap <mattsap@udel.edu>
2014-07-17 21:35:33 -0400

    Testing

---
+++ Music/src/Note.java
@@ -0,0 +1,22 @@
+
+public class Note {
+	private int duration;
+	private int pitch;
+	public Note(int duration, int pitch){
+		this.duration  = duration;
+		this.pitch = pitch;
+	}
+	public int getDuration() {
+		return duration;
+	}
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+	public int getPitch() {
+		return pitch;
+	}
+	public void setPitch(int pitch) {
+		this.pitch = pitch;
+	}
+	
+}
