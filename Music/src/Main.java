f70c39e18bf2c68a385c58e90eacc530dec255fa
mattsap <mattsap@udel.edu>
2014-07-17 22:54:37 -0400

    Read all Midis From Directory

---
+++ Music/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.File;
+
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.MidiMessage;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Track;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		File midiDirectory = new File(".\\src");
+		
+		
+		File[] midiList = midiDirectory.listFiles();
+		for(File file: midiList){
+			if(file.getAbsolutePath().endsWith(".mid")){
+				Song song = Song.parseMidiFile(file);
+				
+			}
+		}
+	}
+}
