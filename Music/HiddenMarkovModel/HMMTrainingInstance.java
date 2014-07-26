ce22c561463c4f3fe5e58256a025ef07378bafba
mattsap <mattsap@udel.edu>
2014-07-26 14:13:40 -0400

    Added getters and setters for Training Instance

--- Music/HiddenMarkovModel/HMMTrainingInstance.java
+++ Music/HiddenMarkovModel/HMMTrainingInstance.java
@@ -3,4 +3,28 @@ public abstract class HMMTrainingInstance<S,V> {
 	private S state;
 	private V observation;
 	private HMMTrainingInstance<S,V> nextInstance;
+	
+	public HMMTrainingInstance(S aState, V anObservation){
+		this.state = aState;
+		this.observation = anObservation;
+	}
+	public S getState() {
+		return state;
+	}
+	public void setState(S state) {
+		this.state = state;
+	}
+	public V getObservation() {
+		return observation;
+	}
+	public void setObservation(V observation) {
+		this.observation = observation;
+	}
+	public HMMTrainingInstance<S, V> getNextInstance() {
+		return nextInstance;
+	}
+	public void setNextInstance(HMMTrainingInstance<S,V> next){
+		this.nextInstance = next;
+	}
+	
 }
